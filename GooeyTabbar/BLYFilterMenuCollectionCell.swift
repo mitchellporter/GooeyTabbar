dbd6b7f2e8cc11696c7af229179a20dc99b26501
Mitchell Porter <mitchellporter@gmail.com>
2016-02-26 19:47:25 -0800

    fixed cell arches, no longer getting cut off

--- GooeyTabbar/BLYFilterMenuCollectionCell.swift
+++ GooeyTabbar/BLYFilterMenuCollectionCell.swift
@@ -13,7 +13,7 @@ class BLYFilterMenuCollectionCell: UICollectionViewCell {
     @IBOutlet weak var iconImageView: UIImageView!
     @IBOutlet weak var label: UILabel!
     
-    let TOPSPACE : CGFloat = 75.375 //留白 // Increase the top space to decrease the height of the elastic effect at top of each cell
+    let TOPSPACE : CGFloat = 44.0 //留白 // Increase the top space to decrease the height of the elastic effect at top of each cell
     var diff: CGFloat = 0.0
     var categoryColor: UIColor!
     
@@ -23,14 +23,14 @@ class BLYFilterMenuCollectionCell: UICollectionViewCell {
 //        layer.borderColor = categoryColor.CGColor
         
         // Add border to top and bottom, but hide it on the sides
-        label.transform = CGAffineTransformMakeTranslation(0, -diff/2.2)
-        iconImageView.transform = CGAffineTransformMakeTranslation(0, -diff/2.2)
+//        label.transform = CGAffineTransformMakeTranslation(0, -diff/2.2)
+//        iconImageView.transform = CGAffineTransformMakeTranslation(0, -diff/2.2)
         
         let path = UIBezierPath()
         path.moveToPoint(CGPoint(x: 0, y: 0)) // top left corner
         path.addLineToPoint(CGPoint(x: self.frame.width, y: 0)) // top right corner
         path.addLineToPoint(CGPoint(x: self.frame.width, y: 0)) // Flat bottom line
-        path.addQuadCurveToPoint(CGPoint(x: 0, y: self.frame.height - TOPSPACE), controlPoint: CGPoint(x: self.frame.width/2, y: self.frame.height - TOPSPACE-diff)) // You had to divide to reduce the number,
+        path.addQuadCurveToPoint(CGPoint(x: 0, y: self.frame.height - TOPSPACE), controlPoint: CGPoint(x: self.frame.width/2, y: self.frame.height - TOPSPACE-diff/0.5)) // You had to divide to reduce the number,
                                                                                                                                                                 // otherwise it stretched to far and was cut off by the next cell
         path.closePath()
         
